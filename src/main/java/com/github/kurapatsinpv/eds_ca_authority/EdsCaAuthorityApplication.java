68b9ee8ac3b009b24c51d77bb64c88bfc8980bc5
Pavel Kuropatin <kuropatin.pv@gmail.com>
2025-12-12 22:25:14 +0300

    Initial commit

---
+++ src/main/java/com/github/kurapatsinpv/eds_ca_authority/EdsCaAuthorityApplication.java
@@ -0,0 +1,12 @@
+package com.github.kurapatsinpv.eds_ca_authority;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EdsCaAuthorityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EdsCaAuthorityApplication.class, args);
+    }
+}
---
+++ src/test/java/com/github/kurapatsinpv/eds_ca_authority/EdsCaAuthorityApplicationTests.java
@@ -0,0 +1,12 @@
+package com.github.kurapatsinpv.eds_ca_authority;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EdsCaAuthorityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
